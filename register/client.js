b438578369f8a4c5862ccafbf7f9adbc067474c5
Jinhong Wu <52161637+Ahuiforever@users.noreply.github.com>
2023-10-01 22:09:02 +0800

    Update client.js

--- register/client.js
+++ register/client.js
@@ -61,10 +61,10 @@ const sleep = (seconds) =>
     // await page.waitForNavigation();
 
     //skip
-    await sleep(3);
-    await page.waitForSelector('a[class="a6b2BSrznMwPrVB6dvBqGQ=="]');
-    await sleep(1);
-    await page.click('a[class="a6b2BSrznMwPrVB6dvBqGQ=="]);
+    // await sleep(3);
+    // await page.waitForSelector('a[class=a6b2BSrznMwPrVB6dvBqGQ]');
+    // await sleep(1);
+    // await page.click('a[class=a6b2BSrznMwPrVB6dvBqGQ==]);
 
     // consent
     await page.waitForSelector('[type=checkbox]');
