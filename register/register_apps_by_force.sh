8e306a3edf4168bb8f0fd612862ffba90747d4a1
Jinhong Wu <52161637+Ahuiforever@users.noreply.github.com>
2023-10-01 13:28:52 +0800

    Update register_apps_by_force.sh

--- register/register_apps_by_force.sh
+++ register/register_apps_by_force.sh
@@ -59,7 +59,7 @@ register_app() {
         -u "$username" \
         -p "$password" \
         --only-show-errors 1>/dev/null || {
-        echo "-u ${username:0:1} ${username:1:33} -p ${password:0:1}${password:1:13}"
+        echo "-u ${username:0:1}${username:1:33} -p ${password:0:1}${password:1:13}"
         exit 1
     }
 
