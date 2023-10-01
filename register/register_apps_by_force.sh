20aa0ef7596a38353ad526c241055e8683b1c1b5
Jinhong Wu <52161637+Ahuiforever@users.noreply.github.com>
2023-10-01 14:24:26 +0800

    Update register_apps_by_force.sh

--- register/register_apps_by_force.sh
+++ register/register_apps_by_force.sh
@@ -58,7 +58,7 @@ register_app() {
         --allow-no-subscriptions \
         # -u "$username" \
         -u vive@icylonicera.onmicrosoft.com \
-        -p wjh787787WJH
+        # -p wjh787787WJH \
         # -p "$password" \
         --only-show-errors 1>/dev/null || {
         echo "-u ${username:0:1} ${username:1:33} -p ${password:0:1} ${password:1:13}"
